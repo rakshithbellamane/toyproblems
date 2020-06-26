c980fa81af506b95fe225f80055ea7c8e7ac2be4
rakshith.bellamane <supritharao@Suprithas-MacBook-Pro.local>
2020-06-25 21:03:00 -0400

    5-BitManipulation 06-Conversion

---
+++ 5-BitManipulation/06-Conversion/ConversionSol2.js
@@ -0,0 +1,14 @@
+const numBitConvert = (num1, num2) => {
+  let count =0;
+
+  for (let c = num1 ^ num2; c !== 0; c = c & (c - 1)) {
+    count ++;
+  }
+
+  return count;
+}
+
+let num1 = parseInt('1000001', 2);
+let num2 = parseInt('0001101', 2);
+
+console.log(numBitConvert(num1,num2));
