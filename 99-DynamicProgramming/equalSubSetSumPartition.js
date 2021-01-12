bc2f3fa5eccfb7ac2c2ec0b5c5458a4e6ff3401a
Rakshith Bellamane <rakshith@Suprithas-MacBook-Pro-2.local>
2021-01-12 06:21:12 -0500

    99-DynamicProgramming equalSubSetSumPartition

--- 99-DynamicProgramming/equalSubSetSumPartition.js
+++ 99-DynamicProgramming/equalSubSetSumPartition.js
@@ -1,22 +1,82 @@
-const hasEqualSum = (part1, part2) => {
-  let sum1 = part1.reduce(((sum, i) => sum + i), 0);
-  let sum2 = part2.reduce(((sum, i) => sum + i), 0);
+// recursive
+const isEPRecursive = (nums, sum, i) => {
+  if (sum === 0) return true;
 
-  return sum1 === sum2;
+  if (i >= nums.length) return false;
+
+  return isEPRecursive(nums, sum, i+1) || isEPRecursive(nums, sum-nums[i], i+1);
 }
 
-const isEqualPartition = (nums, i=0, part1=[], part2=[]) => {
-  if (i >= nums.length && hasEqualSum(part1, part2)) return true;
+const isEqualPartitionSol1 = (nums) => {
+  let sum = nums.reduce((sum, i) => sum+i);
 
-  let newPart11 = [...part1];
-  let newPart12 = [...part1, nums[i]];
+  if (sum%2) return false;
+ 
+  return isEPRecursive(nums, sum/2, 0);
+}
 
-  let newPart21 = [...part2];
-  let newPart22 = [...part2, nums[i]];
+/***************************************************************************************/
 
-  return (isEqualPartition(nums, i+1, newPart11, newPart12) || isEqualPartition(nums, i+1, newPart21, newPart22));
+// recursive with memoization
+const isEPRecursiveMemo = (nums, sum, i, dp) => {
+  if (sum === 0) return true;
+
+  if (i >= nums.length) return false;
+
+  dp[i] = dp[i] || [];
+
+  if (dp[i][sum] === undefined && nums[i] <= sum) {
+    dp[i][sum] = isEPRecursiveMemo(nums, sum-nums[i], i+1, dp);
+  }
+  if (!dp[i][sum])
+    dp[i][sum] = isEPRecursiveMemo(nums, sum, i+1, dp)
+
+  return  dp[i][sum];
+}
+
+const isEqualPartitionSol2 = (nums) => {
+  let sum = nums.reduce((sum, i) => sum+i);
+
+  if (sum%2) return false;
+
+  let dp = [];
+
+  return isEPRecursiveMemo(nums, sum/2, 0, dp);
+}
+
+/**************************************************************************** */
+// bottom up approach
+
+const isEqualPartitionSol3 = (nums) => {
+  let sum = nums.reduce((sum, i) => sum+i);
+
+  if (sum%2) return false;
+
+  let dp = (new Array(nums.length)).fill(new Array(sum/2+1).fill(0));
+
+  for (let i=0; i<nums.length; i++) {
+    dp[i][0] = true;
+  }
+
+  for (let i=0; i<sum/2+1; i++) {
+    if (nums[0] === i) dp[0][i] = true;
+    else dp[0][i] = false;
+  }
+
+  for (let i=1; i<nums.length; i++) {
+    for (let j=1; j<sum/2+1; j++) {
+      dp[i][j] = dp[i-1][j];
+      if (!dp[i][j] && nums[i] <= j) dp[i][j] = dp[i][j-nums[i]];
+    }
+  }
+
+  return dp[nums.length-1][sum/2];
 }
 
 
-let nums = [1,2,3,4];
-console.log(isEqualPartition(nums, 1, [nums[0]], []));
+let nums = [1,2,5,4];
+console.log(`isEqualPartitionSol1: ${isEqualPartitionSol1(nums)}`);
+nums = [1,2,5,4];
+console.log(`isEqualPartitionSol2: ${isEqualPartitionSol2(nums)}`);
+nums = [1,2,5,4];
+console.log(`isEqualPartitionSol2: ${isEqualPartitionSol3(nums)}`);
